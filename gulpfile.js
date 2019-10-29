a52216e57ffb5aad805c471ba57fd21d0f5a541b
Nick Blomberg <nick.blomberg@gmail.com>
2019-10-29 11:01:44 +0700

    Move paths to objects and add copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,51 @@
 const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
 
-const htmlPath = './*.html';
-const sassPath = './sass/**/*.scss';
-const jsPath = './js/**/*.js';
-const imgPath = './img/**/*'
+const paths = {
+    html: ['./*.html'],
+    sass: ['./sass/**/*.scss'],
+    scripts: ['./js/**/*.js'],
+    images: ['./img/**/*'],
+    extras: ['favicon.ico'],
+}
 
 function html() {
-    return src(htmlPath)
+    return src(paths.html)
         .pipe(dest('./dist'))
 }
 
 function css() {
-    return src(sassPath)
+    return src(paths.sass)
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('./dist/css'))
 }
 
 function js() {
-    return src(jsPath)
+    return src(paths.scripts)
         .pipe(dest('./dist/js/'))
 }
 
 function img() {
-    return src(imgPath)
+    return src(paths.images)
         .pipe(dest('./dist/img'))
 }
 
+function copy() {
+    return src(paths.extras)
+        .pipe(dest('./dist'))
+}
+
 function watcher() {
-    watch(htmlPath, html);
-    watch(sassPath, css);
-    watch(jsPath, js);
-    watch(imgPath, img);
+    watch(paths.html, html);
+    watch(paths.sass, css);
+    watch(paths.scripts, js);
+    watch(paths.images, img);
 }
 
 exports.html = html;
 exports.css = css;
 exports.js = js;
 exports.img = img;
+exports.copy = copy;
 exports.watcher = watcher;
-exports.default = parallel(html, css, img, js);
+exports.default = parallel(html, css, img, js, copy);
