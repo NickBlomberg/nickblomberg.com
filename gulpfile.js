f82c5cf7eb95cc19dbec558efb5fa389880d9f83
Nick Blomberg <nick.blomberg@gmail.com>
2019-11-05 11:44:38 +0700

    Automate image optimization with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel, watch } = require('gulp');
+const { src, dest, parallel, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -7,6 +7,10 @@ const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg')
+const pngquant = require('imagemin-pngquant');
+const gulpWebp = require('gulp-webp');
 
 const paths = {
     html: ['./*.html'],
@@ -40,6 +44,18 @@ function js() {
 
 function img() {
     return src(paths.images)
+        .pipe(imagemin([
+            pngquant({ quality: [0.7, 0.7] }),
+            mozjpeg({ quality: 80 })
+        ]))
+        .pipe(dest('./dist/img'))
+}
+
+function webp() {
+    return src(paths.images)
+        .pipe(gulpWebp({
+            quality: 80,
+        }))
         .pipe(dest('./dist/img'))
 }
 
@@ -58,13 +74,14 @@ function watcher() {
     watch(paths.html, html).on('change', browserSync.reload);
     watch(paths.sass, css);
     watch(paths.scripts, js).on('change', browserSync.reload);
-    watch(paths.images, img);
+    watch(paths.images, series(img, webp));
 }
 
 exports.html = html;
 exports.css = css;
 exports.js = js;
 exports.img = img;
+exports.webp = webp;
 exports.copy = copy;
 exports.watcher = watcher;
-exports.default = parallel(html, css, img, js, copy);
+exports.default = parallel(html, css, img, webp, js, copy);
